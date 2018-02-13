2034518c13b6b9c11498acabf3af2eff7c551ee9
toshi shindo <honolulu3310@gmail.com>
2018-02-12 22:27:52 -0500

    modefied google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,9 +21,9 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      // callbackURL: '/auth/google/callback',
-      callbackURL: 'https://mighty-stream-98804.herokuapp.com/auth/google/callback'
-      // proxy: true
+      callbackURL: '/auth/google/callback',
+      // callbackURL: 'https://mighty-stream-98804.herokuapp.com/auth/google/callback'
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
