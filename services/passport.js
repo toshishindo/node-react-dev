37be2298de966e533c287770f56c819bc2af8fb0
toshi shindo <honolulu3310@gmail.com>
2018-02-12 20:24:30 -0500

    Added authentication

---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  // 1st argument is error. This id is not profile.id, it is mongo id
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already hav a record with the given profile ID
+          // done(error, user record)
+          done(null, existingUser);
+        } else {
+          //we dont' have a user record with this ID, make a new ID
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
