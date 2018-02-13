7bbfce252b596c83d7ff14b268ea91f397a71456
toshi shindo <honolulu3310@gmail.com>
2018-02-12 21:28:55 -0500

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // prod.js - production keys here !!
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE.CLIENT_SECRET,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY 
 };
